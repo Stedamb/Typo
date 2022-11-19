3891a2e926e4dc38b86fb82341a963eaebc8c0b4
Stefano <stedamb@gmail.com>
2022-11-19 20:21:18 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let activeIndex = 0;
+
+const slides = document.getElementsByClassName("page");
+
+function handleLeftClick() {
+
+    console.log("left clicked");
+
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+
+    const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentSlide.dataset.status = "after";
+
+    nextSlide.dataset.status = "becoming-active-from-before";
+
+    setTimeout(() => {
+        nextSlide.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
+
+const handleRightClick = () => {
+
+    console.log("right clicked");
+
+    const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+
+    const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentSlide.dataset.status = "before";
+
+    nextSlide.dataset.status = "becoming-active-from-after";
+
+    setTimeout(() => {
+        nextSlide.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
